7a0dbbf6c0127632c0d52f81fa9d456372f881af
Neil Girardi <neil.girardi@gmail.com>
2012-03-04 20:48:30 -0500

    latest CSS updates

--- neilgirardi/application/views/includes/nav.php
+++ neilgirardi/application/views/includes/nav.php
@@ -1,15 +1,17 @@
-<div id="nav" class="grid_12">
-    <nav>
-        <ul class="play">
-            <li><a href="portfolio">Portfolio</a></li>
-            <li><a href="resume">Resume</a></li>
-            <li><a href="bio">Bio</a></li>
-            <li><a href="contact">Contact</a></li>
-        </ul>
-    </nav>
-    
-    <div id="searchBox" class="pull_right">
-        <input id="searchInput" class="grid_3 rounded" type="text" /> 
+<div id="nav">
+    <div class="grid_12 centered">
+        <nav>
+            <ul class="play">
+                <li><a href="portfolio">Portfolio</a></li>
+                <li><a href="resume">Resume</a></li>
+                <li><a href="bio">Bio</a></li>
+                <li><a href="contact">Contact</a></li>
+            </ul>
+        </nav>
+        
+        <div id="searchBox" class="pull_right">
+            <input id="searchInput" class="grid_3 rounded" type="text" /> 
+        </div>
+        
     </div>
-    
 </div>
--- neilgirardi/application/views/portfolio.php
+++ neilgirardi/application/views/portfolio.php
@@ -1,10 +1,21 @@
  <div id="siteContainer" class="centered">
-    <div class="row">
-        <h1 class="arimo">This is a heading</h1>
-        <p class="grid_12">Quisque pellentesque euismod massa, eget vehicula sapien ullamcorper <a href="/">feugiat</a>. Vivamus nec turpis sit amet augue facilisis sollicitudin. Praesent tristique mollis mauris ultricies hendrerit. Aenean tincidunt, sapien id ullamcorper suscipit, purus metus lacinia ante, quis accumsan velit leo a eros. Praesent id tellus ut leo fringilla dictum vitae a tortor. Nam et libero lectus, in vestibulum est. Aenean vitae dapibus purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse dictum volutpat neque sed mattis. Sed ac neque mauris. Suspendisse potenti. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum interdum nunc quis nunc imperdiet commodo. Nunc vitae ipsum eros, viverra suscipit felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-        <h1 class="arimo">This is another Heading</h1>
-        <p>Vivamus a ante odio, fermentum adipiscing tellus. Aliquam euismod euismod ipsum id bibendum. In mollis mattis orci in mollis. Praesent in dolor eu magna fringilla facilisis at eu tortor. Ut pharetra pharetra arcu, nec egestas libero laoreet vel. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam id velit id urna facilisis commodo. Nullam fringilla dictum leo in interdum. Mauris convallis risus id orci lacinia nec blandit enim ultrices. </p>
+     
+     <div class="row">
+         <h1>This is the main heading</h1>         
+        <p class="grid_6">Vivamus a ante odio, fermentum adipiscing tellus. Aliquam euismod euismod ipsum id bibendum. In mollis mattis orci in mollis. Praesent in dolor eu magna fringilla facilisis at eu tortor. Ut pharetra pharetra arcu, nec egestas libero laoreet vel. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam id velit id urna facilisis commodo. Nullam fringilla dictum leo in interdum. Mauris convallis risus id orci lacinia nec blandit enim ultrices.</p>
+        <p class="grid_6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse condimentum porta lacus, ac suscipit sem iaculis et. Vestibulum molestie dignissim dolor, vel congue eros luctus ultricies. Quisque porta adipiscing pulvinar. Sed in arcu a erat lobortis elementum. Cras lobortis tincidunt diam vitae bibendum. Vestibulum facilisis nulla at nisi mollis ullamcorper. Nunc elementum gravida aliquam. Quisque tempor ligula at nunc tincidunt a dapibus urna auctor. 
+        </p>
+         <div id="hero-box" class="grid_12 bordres_thin">
+         </div>
      </div>
+    
+    <div class="row">
+        <h2 class="arimo">This is a heading</h1>
+        <p class="grid_6">Quisque pellentesque euismod massa, eget vehicula sapien ullamcorper <a href="/">feugiat</a>. Vivamus nec turpis sit amet augue facilisis sollicitudin. Praesent tristique mollis mauris ultricies hendrerit. Aenean tincidunt, sapien id ullamcorper suscipit, purus metus lacinia ante, quis accumsan velit leo a eros. Praesent id tellus ut leo fringilla dictum vitae a tortor. Nam et libero lectus, in vestibulum est. Aenean vitae dapibus purus. </p>
+        <p class="grid_6">Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse dictum volutpat neque sed mattis. Sed ac neque mauris. Suspendisse potenti. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum interdum nunc quis nunc imperdiet commodo. Nunc vitae ipsum eros, viverra suscipit felis. Lorem ipsum dolor sit amet, consectetur adipiscing. </p>
+     </div>
+     
      <div id="portfolio_sliders" class="row">
          <div class="box grid_4 borders_thin"></div>
          <div class="box grid_4 borders_thin"></div>
